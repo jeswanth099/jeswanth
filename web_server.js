ffc78e279e68313a0fd75859e35d76b6e05c55e2
jeswanth099 <jeswanth.venkatesh@evanam.com>
2025-03-27 21:18:21 +0530

    Add files via upload

---
+++ web_server.js
@@ -0,0 +1,77 @@
+const express=require('express')
+const app = express()
+const path=require('path')
+const cors=require('cors')
+const logEvent = require('./middleware1/logEvents.js')
+const errosHandel=require('./middleware1/errosHandler.js')
+const PORT=process.env.PORT|| 3500;
+// custom middleware
+app.use ((req,res,next)=>{
+    logEvent(`${req.method}\t${req.headers.origin}\t${req.url}`,'reqLog.txt')
+    console.log(`${req.method} ${req.path}`)
+    next()
+})
+
+app.use(cors())
+const whitelist =('https://www.google.com/','https://127.0.0.1.5500','https://localhost:3500')
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1|| !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionSuccessStatius:200
+}
+app.use(cors(corsOptions))
+
+// bulid in middleware
+app.use(express.urlencoded({extended:false}))// url encoded middleware for to  use user data
+app.use(express.json())// to handle json files
+app.use('/',express.static(path.join(__dirname,'./public')))//to access public folder
+app.use('/subdir',express.static(path.join(__dirname,'./public')))
+app.use('/',require('./routes/root'))
+app.use('/subdir',require('./routes/subdir'))
+
+// custom middleware
+app.use ((req,res,next)=>{
+    console.log(`${req.method} ${req.path}`)
+    next()
+})
+
+
+
+/*app.get('/hello(.html)?',(req,res,next)=>
+{console.log('hello.html page loadeding'); 
+next();},
+(res,req,next)=>{console.log('hello page')})
+const one=(req,res,next)=>{
+    console.log('one');
+    next();
+}
+const two=(req,res,next)=>{
+    console.log('two');
+    next();
+}
+const three=(req,res,)=>{
+    console.log('one'); 
+    res.send('Finished')   
+}
+app.get('/chain(.html)?',[one,two,three])*/
+app.get('/*',(req,res)=>
+{res.status(404).sendFile(path.join(__dirname,'views','404.html'));}
+)
+app.all('*',(req,res)=>{
+res.status(404);
+if(req.accepts('html')){
+    res.sendFile(path.jont(__dirname,'views','404.html'));
+}else if(req.accepts('json')){
+    res.json({"erroe":"404 not Found"});
+}else
+{
+    res.type('txt').send('404 not Found')
+}
+})
+app.use(errosHandel)
+app.listen(PORT,() => console.log(`server running on port ${PORT}`));
